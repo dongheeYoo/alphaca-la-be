4f9939ed5314c85330540ac882b671fe3e555c03
yoodonghee <yoodhajou0616@gmail.com>
2024-04-17 02:07:40 +0900

    set up username and pwd

--- src/app/mongodb/config/config.js
+++ src/app/mongodb/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   //local
   //url: "mongodb://localhost:27017/admin",
   //server
-  url: "mongodb://115.85.180.49:27017/admin",
+  url: "mongodb://donghee:!Hb78236208@115.85.180.49:27017/admin",
 };
